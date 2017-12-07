00a5fb12396a463a0f34d41114dae25b81345836
tanya525625 <tanya_postcrossing@mail.ru>
2017-12-07 23:02:16 +0300

    Client manager added

---
+++ Core/CM/IClientManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core
+{
+    namespace CM
+    {
+        interface IClientManager : IControler, IControlable, ILoggable, IEventBaseUser, IPositionBaseReader
+        {
+            ICollection<IClient> clientsBase;
+            void SendCommand(IControlable target, ICommand command);
+            IPositionBaseReader positionBase;
+            IEventBase eventsBase;
+            
+        }
+    }
+}
